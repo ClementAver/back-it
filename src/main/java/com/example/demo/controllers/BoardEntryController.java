d94736bc573cabc2ad10467074ecf449b220e84c
clement.aver <clement.aver@yahoo.fr>
2026-09-25 18:02:31 +0200

    [phrasing]

--- src/main/java/com/example/demo/controllers/BoardEntryController.java
+++ src/main/java/com/example/demo/controllers/BoardEntryController.java
@@ -50,7 +50,7 @@ public class BoardEntryController {
     public Map<String, List<BoardEntryResponse>> readBoardEntries(@PathParam("board_id") @Min(value = 1, message = "L'identifiant doit être égal ou supérieur à un (1).") Integer boardId) {
         List<BoardEntryResponse> boardEntryList = boardEntryService.readBoardEntries(boardId);
         Map<String, List<BoardEntryResponse>> response = new HashMap<>();
-        response.put("board_entries", boardEntryList);
+        response.put("boardEntries", boardEntryList);
         return response;
     }
 }
